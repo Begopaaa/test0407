795dd296df938d566dd00e1a6e42752f8aa80444
DHong <eoghd7630@naver.com>
2022-04-05 10:11:40 +0900

    add your

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,5 @@ public class HelloWorld {
 		System.out.println("Hello world");
 		System.out.println("Hello his world");
 		System.out.println("Hello your World");
-		System.out.println("Hello her world");
 	}
-	
+}
