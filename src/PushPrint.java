96fe604b5243ffcf111cbbf9f78ebcde42e267f1
DHong <eoghd7630@naver.com>
2022-04-07 11:11:10 +0900

    added 'test'

--- src/PushPrint.java
+++ src/PushPrint.java
@@ -3,5 +3,6 @@ public class PushPrint {
 	public static void main(String[] arg) {
 		System.out.println("Add Java file");
 		System.out.println("hello in local");
+		System.out.println("test");
 	}
 }
